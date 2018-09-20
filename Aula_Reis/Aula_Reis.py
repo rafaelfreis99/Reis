71cd73ec51331b1474423e1f6e8ae45d03f01a72
rafael.freis2 <reisrafael30@gmail.com>
2018-09-20 20:11:48 -0300

    Teste 01

---
+++ Aula_Reis/Aula_Reis.py
@@ -0,0 +1,16 @@
+import math
+
+a = int(input("Informe valor de a: "))
+b = int(input("Informe valor de b: "))
+c = int(input("Informe valor de c: "))
+
+delta = b * b - 4 * a * c
+print("delta: ", delta)
+
+if delta >= 0:
+    x1 = (-b + math.sqtr((delta))) / (2 * a)
+    x2 = (-b + math.sqtr((delta))) / (2 * a)
+    print("Raiz x1: ", x1)
+    print("Raiz x2: ", x2)
+else:
+    print("Não existem raízes reais")
